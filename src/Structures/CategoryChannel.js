a0a6032156e361832a28a5b76b19637bf41927e3
System Administrator <rekenmarkie@gmail.com>
2018-06-14 18:02:21 +0200

    Debugging

--- src/Structures/CategoryChannel.js
+++ src/Structures/CategoryChannel.js
@@ -5,7 +5,9 @@ const GuildChannel = require("./GuildChannel.js");
 class CategoryChannel extends GuildChannel {
   constructor(data, client) {
     super(data, client);
-    this.children = this.guild.channels.filter(channel => channel.parentID === this.id);
+  }
+  get children() {
+    return this.guild.channels.filter(channel => channel.parentID === this.id);
   }
 }
 
